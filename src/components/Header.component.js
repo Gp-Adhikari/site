4fd235f2d9ee4e43c51aefdbd74b63e599c4d095
Gp Adhikari <adhikari.gp232@gmail.com>
2022-01-04 12:19:56 +0545

    header for desktops

--- src/components/Header.component.js
+++ src/components/Header.component.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../img/logo.svg";
 
 const Header = () => {
   return (
-    <div>
-      <p>Hi</p>
-    </div>
+    <header>
+      <img src={logo} alt="logo" />
+
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/services">Services</NavLink>
+        <NavLink to="/portfolio">Portfolio</NavLink>
+        <NavLink to="/carrers">Carrers</NavLink>
+        <NavLink to="/contact">Contact Us</NavLink>
+      </nav>
+    </header>
   );
 };
 
