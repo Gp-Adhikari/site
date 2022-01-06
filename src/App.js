f86610be9b5fb58d9a94092284b0a865a0045b23
Gp-Adhikari <adhikari.gp232@gmail.com>
2022-01-06 19:34:28 +0545

    added suspense and lazy for route based code splitting

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Footer from "./components/Footer.component";
-import Header from "./components/Header.component";
-import Home from "./pages/Home.page";
+
+const Footer = lazy(() => import("./components/Footer.component"));
+const Header = lazy(() => import("./components/Header.component"));
+const Home = lazy(() => import("./pages/Home.page"));
 
 const App = () => {
   return (
     <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <Suspense fallback={<p>Loading</p>}>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </Suspense>
     </>
   );
 };
