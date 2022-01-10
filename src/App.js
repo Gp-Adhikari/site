3792915d5b621a9a4d828519637e0027064d7d7f
parbati <manandharparbati456@gmail.com>
2022-01-10 11:10:26 +0545

    careeritem completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header.component";
-import Title from "./components/Title.component";
+import Title from "./components/Title.component"; 
+import Banner from "./components/Banner.component";
+import CareerItem from "./components/CareerItem.component";
 
 const App = () => {
   return (
@@ -9,7 +11,9 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" exact element={<Header />} />
+          <Route path ="/banner" exact element={<Banner/>}/>
           <Route path="/Title" exact element={<Title />} />
+          <Route path="/careeritem/:id" exact element={<CareerItem/>}/>
         </Routes>
       </Router>
     </>
