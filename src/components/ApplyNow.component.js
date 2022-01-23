8ef0eec70114728aa15ef43d2fd317082b4eecc4
birendradewal <birendradewal123@gmail.com>
2022-01-23 11:14:35 +0545

    career pageCompleted

--- src/components/ApplyNow.component.js
+++ src/components/ApplyNow.component.js
@@ -11,14 +11,14 @@ const ApplyNow = () => {
     <>
       <div className="applyNowSection">
         <div className="applyNowContainer">
-          <div className="applyNow-ReactJsDev">
+          <div className="vacancyTitle">
             <h3>Apply Now!</h3>
             <h1>ReactJS Developer</h1>
             <h3>Corporate Title: Front-End Developer</h3>
             <p>No. of Vacancies: 1</p>
             <p>Posted Date: April 12, 2021</p>
           </div>
-          <div className="applyNow-Requirement">
+          <div className="vacancyRequirement">
             <h1>Requirements:</h1>
             <ul type="disc">
               <li>Must have 1 year experience with React.</li>
@@ -27,14 +27,49 @@ const ApplyNow = () => {
               <li>Knowledge of UI/UX is plus.</li>
             </ul>
           </div>
-          <div className="applyNow-Salary">
+          <div className="jobSalary">
             <p>Salary:</p>
             <p>Negotiable ( Depends on your skills and experience ).</p>
           </div>
           <h1>Apply for this Job</h1>
-          <div className="applyForJob-Table">
-            <form action="#">
-              {/* <table>
+          <form action="#">
+            <div className="formInputWrapper">
+              <div className="formInput">
+                <p>Full Name</p>
+                <input type="text" />
+              </div>
+              <div className="formInput">
+                <p>E-mail</p>
+                <input type="email" />
+              </div>
+            </div>
+            <div className="formInputWrapper">
+              <div className="formInput">
+                <p>Phone Number</p>
+                <input type="number" />
+              </div>
+              <div className="formInput">
+                <p>Current Address</p>
+                <input type="text" />
+              </div>
+            </div>
+            <div className="formInputWrapper">
+              <div className="formInput formInputMessage">
+                <p>Message</p>
+                <textarea id="" rows="12"></textarea>
+              </div>
+            </div>
+            <div className="formInputWrapper">
+              <div className="formInput">
+                <p>Please attach your CV here ( In .pdf format )</p>
+                <CustomFileDrag
+                  handleChange={(e) => setPdf(e.target.value)}
+                  getData={getPdf}
+                  data={pdf}
+                />
+              </div>
+            </div>
+            {/* <table>
                 <thead></thead>
                 <tbody>
                   <tr>
@@ -87,8 +122,7 @@ const ApplyNow = () => {
                   </tr>
                 </tbody>
               </table> */}
-            </form>
-          </div>
+          </form>
         </div>
       </div>
     </>
